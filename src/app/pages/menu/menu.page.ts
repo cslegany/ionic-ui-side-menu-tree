022c95008aa27c329d338f709faf42b90f1af6ac
Csaba Legány <cslegany@synerinsoft.com>
2020-10-25 11:35:27 +0100

    init

---
+++ src/app/pages/menu/menu.page.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-menu',
+  templateUrl: './menu.page.html',
+  styleUrls: ['./menu.page.scss'],
+})
+export class MenuPage implements OnInit {
+
+  pages = [
+    {
+      title: 'Main',
+      url: '/menu/main',
+      icon: 'home',
+      open: false
+    },
+    {
+      title: 'Cool Frameworks',
+      children: [
+        {
+          title: 'Ionic',
+          url: '/menu/ionic',
+          icon: 'logo-ionic',
+          open: false
+        },
+        {
+          title: 'Flutter',
+          url: '/menu/flutter',
+          icon: 'logo-google',
+          open: false
+        }
+      ],
+      open: false
+    }
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
